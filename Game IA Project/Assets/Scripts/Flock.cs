052805f52aba3d04e17677a4995e7097480a6afc
Hekbas <34044086+Hekbas@users.noreply.github.com>
2023-10-08 00:55:00 +0200

    Fixed NavMesh, Modified Flock script (need to fix)

--- Game IA Project/Assets/Scripts/Flock.cs
+++ Game IA Project/Assets/Scripts/Flock.cs
@@ -1,66 +1,158 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Flock : MonoBehaviour
 {
     public FlockingManager flockingManager;
-    float speed;
 
-    // Use this for initialization
-    void Start()
+    private float speed;
+    private Vector3 moveDirection;
+    public Vector3 baseRotation;
+    public Vector3 acceleration;
+    public Vector3 velocity;
+
+    private Vector3 Position
     {
-        speed = Random.Range(flockingManager.minSpeed, flockingManager.maxSpeed);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(0, 0, Time.deltaTime * speed);
-        ApplyRules();
-    }
-
-    void ApplyRules()
-    {
-        GameObject[] gos = flockingManager.allFish;
-
-        Vector3 vcentre = Vector3.zero;
-        Vector3 vavoid = Vector3.zero;
-        float gSpeed = 0.01f;
-        float nDistance;
-        int groupSize = 0;
-
-        foreach (GameObject go in gos)
+        get
         {
-            if (go != this.gameObject)
-            {
-                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
-
-                if (nDistance <= flockingManager.neighbourDistance)
-                {
-                    vcentre += go.transform.position;
-                    groupSize++;
-
-                    if (nDistance < 1.0f)
-                    {
-                        vavoid = vavoid + (this.transform.position - go.transform.position);
-                    }
-
-                    Flock anotherFlock = go.GetComponent<Flock>();
-                    gSpeed = gSpeed + anotherFlock.speed;
-                }
-            }
+            return gameObject.transform.position;
         }
-
-        if (groupSize > 0)
+        set
         {
-            vcentre = vcentre / groupSize;
-            speed = gSpeed / groupSize;
+            gameObject.transform.position = value;
+        }
+    }
 
-            Vector3 direction = (vcentre + vavoid) - transform.position;
+    private void Start()
+    {
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle) + baseRotation);
+        velocity = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), Mathf.Cos(angle));
+    }
 
-            if (direction != Vector3.zero)
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), flockingManager.rotationSpeed * Time.deltaTime);
+    private void Update()
+    {
+        var boidColliders = Physics.OverlapSphere(Position, flockingManager.neighborhoodRadius);
+        var boids = boidColliders.Select(o => o.GetComponent<Flock>()).ToList();
+        boids.Remove(this);
+
+        Flocking(boids);
+        UpdateVelocity();
+        UpdatePosition();
+        UpdateRotation();
+
+        Bounds();
+    }
+
+    private void Flocking(IEnumerable<Flock> boids)
+    {
+        var alignment = Alignment(boids);
+        var separation = Separation(boids);
+        var cohesion = Cohesion(boids);
+
+        acceleration = flockingManager.alignmentAmount * alignment + flockingManager.cohesionAmount * cohesion + flockingManager.separationAmount * separation;
+    }
+
+    public void UpdateVelocity()
+    {
+        velocity += acceleration;
+        velocity = LimitMagnitude(velocity, flockingManager.maxSpeed);
+    }
+
+    private void UpdatePosition()
+    {
+        Position += velocity * Time.deltaTime;
+    }
+
+    private void UpdateRotation()
+    {
+        var angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle) + baseRotation);
+    }
+
+    private Vector3 Alignment(IEnumerable<Flock> boids)
+    {
+        var velocitySum = Vector3.zero;
+        if (!boids.Any()) return velocitySum;
+
+        foreach (var boid in boids)
+        {
+            velocitySum += boid.velocity;
+        }
+        velocitySum /= boids.Count();
+
+        var steer = Steer(velocitySum.normalized * flockingManager.maxSpeed);
+        return steer;
+    }
+
+    private Vector3 Cohesion(IEnumerable<Flock> boids)
+    {
+        if (!boids.Any()) return Vector3.zero;
+
+        var sumPositions = Vector3.zero;
+        foreach (var boid in boids)
+        {
+            sumPositions += boid.Position;
+        }
+        var average = sumPositions / boids.Count();
+        var direction = average - Position;
+
+        var steer = Steer(direction.normalized * flockingManager.maxSpeed);
+        return steer;
+    }
+
+    private Vector3 Separation(IEnumerable<Flock> boids)
+    {
+        var direction = Vector3.zero;
+        boids = boids.Where(o => DistanceTo(o) <= flockingManager.neighborhoodRadius / 2);
+        if (!boids.Any()) return direction;
+
+        foreach (var boid in boids)
+        {
+            var difference = Position - boid.Position;
+            direction += difference.normalized / difference.magnitude;
+        }
+        direction /= boids.Count();
+
+        var steer = Steer(direction.normalized * flockingManager.maxSpeed);
+        return steer;
+    }
+
+    private Vector3 Steer(Vector3 desired)
+    {
+        var steer = desired - velocity;
+        steer = LimitMagnitude(steer, flockingManager.maxForce);
+
+        return steer;
+    }
+
+    private float DistanceTo(Flock boid)
+    {
+        return Vector3.Distance(boid.transform.position, Position);
+    }
+
+    private Vector3 LimitMagnitude(Vector3 baseVector, float maxMagnitude)
+    {
+        if (baseVector.sqrMagnitude > maxMagnitude * maxMagnitude)
+        {
+            baseVector = baseVector.normalized * maxMagnitude;
+        }
+        return baseVector;
+    }
+
+    void Bounds()
+    {
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+
+        // Keep the fish inside bounds
+        Vector3 newPosition = transform.position;
+
+        if (newPosition.x < -flockingManager.swimLimits.x || newPosition.x > flockingManager.swimLimits.x ||
+            newPosition.y < -flockingManager.swimLimits.y || newPosition.y > flockingManager.swimLimits.y ||
+            newPosition.z < -flockingManager.swimLimits.z || newPosition.z > flockingManager.swimLimits.z)
+        {
+            moveDirection = -moveDirection; // Reverse direction
         }
     }
 }
--- Game IA Project/Assets/Scripts/FlockingManager.cs
+++ Game IA Project/Assets/Scripts/FlockingManager.cs
@@ -10,16 +10,43 @@ public class FlockingManager : MonoBehaviour
     public GameObject[] allFish;
     public Vector3 swimLimits = new Vector3(5, 5, 5);
 
-    [Header("Fish Settings")]
-    [Range(0.0f, 5.0f)]
-    public float minSpeed = 1;
-    [Range(0.0f, 5.0f)]
-    public float maxSpeed = 3;
+    //[Header("Fish Settings")]
+    //[Range(0.0f, 5.0f)]
+    //public float minSpeed = 1;
+    //[Range(0.0f, 5.0f)]
+    //public float maxSpeed = 3;
+    //[Range(1.0f, 10.0f)]
+    //public float neighbourDistance = 2;
+    //[Range(0.0f, 5.0f)]
+    //public float rotationSpeed = 2;
 
-    [Range(1.0f, 10.0f)]
-    public float neighbourDistance = 2;
-    [Range(0.0f, 5.0f)]
-    public float rotationSpeed = 2;
+    //[Header("Flock Settings")]
+    //[Range(0.0f, 5.0f)]
+    //public float alignmentWeight = 1.0f;
+    //[Range(0.0f, 5.0f)]
+    //public float cohesionWeight = 1.0f;
+    //[Range(0.0f, 5.0f)]
+    //public float separationWeight = 1.0f;
+
+    [Header("Flock Settings")]
+    [Range(0, 10)]
+    public float maxSpeed = 1f;
+
+    [Range(.1f, .5f)]
+    public float maxForce = .03f;
+
+    [Range(1, 10)]
+    public float neighborhoodRadius = 3f;
+
+    [Range(0, 3)]
+    public float separationAmount = 1f;
+
+    [Range(0, 3)]
+    public float cohesionAmount = 1f;
+
+    [Range(0, 3)]
+    public float alignmentAmount = 1f;
+
 
     // Use this for initialization
     void Start()
@@ -33,7 +60,12 @@ public class FlockingManager : MonoBehaviour
                                                                 Random.Range(-swimLimits.z, swimLimits.z));
 
             allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
-            //allFish[i].GetComponent<Flock>().flockingManager = this;
+
+            Flock flockComponent = allFish[i].GetComponent<Flock>();
+            if (flockComponent != null)
+            {
+                flockComponent.flockingManager = this;
+            }
         }
 
     }
