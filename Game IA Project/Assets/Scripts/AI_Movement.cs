56ab1f4be2427b5134e4062db6a8ef88abf88cb6
Hekbas <34044086+Hekbas@users.noreply.github.com>
2023-10-08 01:05:58 +0200

    Readded AI_Movement

---
+++ Game IA Project/Assets/Scripts/AI_Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_Movement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+    public float maxJump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Seek();
+    }
+    void Seek()
+    {
+        agent.destination = target.transform.position;
+    }
+}
